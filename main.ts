e4b6c2a1325a202f12c4bcb23d12b330a96ed368
bluesky2106 <60764284+bluesky2106@users.noreply.github.com>
2021-02-05 21:56:33 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
+    serial.writeNumber(receivedNumber)
+})
+serial.onDataReceived(serial.delimiters(Delimiters.Hash), function () {
+    cmd = serial.readUntil(serial.delimiters(Delimiters.Hash))
+    radio.sendString(cmd)
+})
+let cmd = ""
+radio.setGroup(77)
+serial.redirectToUSB()
